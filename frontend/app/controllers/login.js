c5f6376bb013e0a4bdfc10ad0d61b9beab41d6bf
mulicvedad <mulic.vedad@gmail.com>
2017-05-07 03:35:55 +0200

    Dodan logout i osnovna validacija za login (frontend)

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -2,12 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	session: Ember.inject.service(),
-
+	validationError: "",
+	authenticationError: "",
 	actions: {
 		authenticate: function() {
 			var credentials = this.getProperties('identification', 'password'), authenticator = 'authenticator:jwt';
+			
+			this.set('validationError','');
 
-			this.get('session').authenticate(authenticator, credentials);
+			if(!credentials.identification || !credentials.password){
+				this.set('validationError','Polja za unos ne smiju biti prazna');
+				return;
+			}
+			
+			if(!credentials.identification.match(/^[a-z0-9]+$/i)){
+				this.set('validationError','Korisnicko ime moze sadrzavari iskljucivo alfanumericke znakove.');
+				return;
+			}
+
+			if(credentials.password.length<=6){
+				this.set('validationError','Lozinka mora biti duza od 6 znakova.');
+				return;
+			}
+			this.get('session').authenticate(authenticator, credentials).catch((reason)=>{
+				this.set('authenticationError', reason.error || reason);
+			});
 		}
 	}
 });

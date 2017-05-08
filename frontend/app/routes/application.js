e083b05653418749ad41f3a71a3946591a29ff2e
mulicvedad <mulic.vedad@gmail.com>
2017-05-08 17:01:23 +0200

    Male promjene
    Prelazak sa ember-simple-auth-token na custom implementaciju uz pomoc
    ember-simple-auth.

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-const { service } = Ember.inject;
-
 export default Ember.Route.extend(ApplicationRouteMixin, {
+	session: Ember.inject.service(),
 	actions: {
+		//poziva se za logout
 		invalidateSession: function(){
 			this.get('session').invalidate();
-		}
+		},
 	}
 });

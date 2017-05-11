deecf4272a66f944bde4d3cae4e51b674fd4370a
mulicvedad <mulic.vedad@gmail.com>
2017-05-11 03:47:42 +0200

    Provjera logina i uloga korisnika povezana sa bazom (tabela User)

--- src/main/java/com/ws1001/security/TokenAuthenticationService.java
+++ src/main/java/com/ws1001/security/TokenAuthenticationService.java
@@ -2,19 +2,30 @@ package com.ws1001.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security
-            .authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
-
+import java.util.ArrayList;
+import java.util.Collection;
 import java.io.IOException;
 
 import static java.util.Collections.emptyList;
 
+import com.ws1001.repositories.UserRepository;
+import com.ws1001.models.User;
+
 class TokenAuthenticationService {
+
+  private static UserRepository userRepository;
+
   static final long EXPIRATIONTIME = 864_000_000; // 10 days
   static final String SECRET = "ThisIsASecret";
   static final String TOKEN_PREFIX = "Bearer";
@@ -24,32 +35,45 @@ class TokenAuthenticationService {
 
   //ovdje cemo morati dodati uloge u odgovor da bi na klijentskoj strani znali privilegije
   static void addAuthentication(HttpServletResponse res, String username) throws IOException{
-    String JWT = Jwts.builder()
-        .setSubject(username)
-        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
-        .signWith(SignatureAlgorithm.HS512, SECRET)
-        .compact();
-    res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
-    res.addHeader(HEADER_CORS, RS_ORIGIN);
-    String str1 = "{ \"jwt\" : ";
-    String str2 = "\"" + JWT + "\" }";
-    res.getWriter().write(str1 + str2);
+      String JWT = Jwts.builder()
+          .setSubject(username)
+          .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+          .signWith(SignatureAlgorithm.HS512, SECRET)
+          .compact();
+      res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+      res.addHeader(HEADER_CORS, RS_ORIGIN);
+      String str1 = "{ \"jwt\" : ";
+      String str2 = "\"" + JWT + "\" }";
+      res.getWriter().write(str1 + str2);
   }
 
   static Authentication getAuthentication(HttpServletRequest request) {
-    String token = request.getHeader(HEADER_STRING);
-    if (token != null) {
-      // parse the token.
-      String user = Jwts.parser()
-          .setSigningKey(SECRET)
-          .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-          .getBody()
-          .getSubject();
+      
+      ServletContext servletContext = request.getServletContext();
+      WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(servletContext);
+      userRepository = webApplicationContext.getBean(UserRepository.class);
 
-      return user != null ?
-          new UsernamePasswordAuthenticationToken(user, null, emptyList()) :
+      String token = request.getHeader(HEADER_STRING);
+      if (token != null) {
+    
+        String userReq = Jwts.parser()
+            .setSigningKey(SECRET)
+            .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+            .getBody()
+            .getSubject();
+
+      User user = userRepository.findByUsername(userReq);
+
+      Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+      if (user != null) {
+          grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole()));
+      }     
+
+      return userReq != null ?
+          new UsernamePasswordAuthenticationToken(userReq, null, grantedAuthorities) : //emptyList()
           null;
-    }
-    return null;
+      }
+      return null;
   }
 }

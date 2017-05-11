ce18199efd604e177aa99da8676d3648ef6ec56c
Sanil Musić <music.sanil@gmail.com>
2017-05-11 17:54:10 +0200

    Sitne ispravke

--- frontend/app/routes/users.js
+++ frontend/app/routes/users.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 //ovo znaci da samo onaj korisnik koji je prijavljen moze pristupiti ruti /korisnici
 //svrha: testiranje uspjesnosti logina
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend();

df57d999332dd3636fd73a21746e2ce99f900fe1
liliumdev <ahmed.popovic@gmail.com>
2017-06-09 19:55:06 +0200

    Fixed issue #23

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     flashMessages: Ember.inject.service(),
+    disabled: false,
 
     authenticate: function(credentials) {
         var authenticator = 'authenticator:jwt';
@@ -11,6 +12,7 @@ export default Ember.Controller.extend({
 
     actions: {
         login: function(credentials, doRedirect) {
+            this.set('disabled', true);
             const flashMessages = Ember.get(this, 'flashMessages');
             var self = this;
             this.authenticate(credentials).then(function(value) {
@@ -19,8 +21,10 @@ export default Ember.Controller.extend({
                 }
                 flashMessages.success("Uspješno prijavljen!");
             }.bind(doRedirect), function(reason) {
+                flashMessages.clearMessages();
                 flashMessages.danger("Pogrešni podaci.");
-            });
+                this.set('disabled', false);
+            }.bind(this));
         },
 
         loginNormal: function() {

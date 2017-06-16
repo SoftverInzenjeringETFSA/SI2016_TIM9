f626dd26616a5308451554ce954a00cab5290ee8
liliumdev <ahmed.popovic@gmail.com>
2017-06-16 04:23:34 +0200

    deployment fixes

--- src/main/java/com/ws1001/WebAppConfig.java
+++ src/main/java/com/ws1001/WebAppConfig.java
@@ -3,22 +3,18 @@ package com.ws1001;
 import org.resthub.web.springmvc.router.RouterConfigurationSupport;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "com.ws1001.controllers")
 public class WebAppConfig extends RouterConfigurationSupport {
     @Override
     public List<String> listRouteFiles() {
-
+        String routes = getClass().getClassLoader().getResource("routes.conf").toString();
         List<String> routeFiles = new ArrayList<String>();
-        routeFiles.add("classpath:routes.conf");
+        routeFiles.add(routes);
         return routeFiles;
     }
 }

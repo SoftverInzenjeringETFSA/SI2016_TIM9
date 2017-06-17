a8bed41d9678774a9aeb0d7a9485919b94593019
liliumdev <ahmed.popovic@gmail.com>
2017-06-17 14:27:04 +0200

    vidjecemo

--- src/main/java/com/ws1001/RsApplication.java
+++ src/main/java/com/ws1001/RsApplication.java
@@ -2,6 +2,8 @@ package com.ws1001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
